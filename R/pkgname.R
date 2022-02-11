b3269c7f8378e8eb5fa62135711cedf41a486b51
Masiar Novine <novinefarahb@uni-potsdam.de>
2022-02-11 21:27:37 +0100

    Clean up

--- R/pkgname.R
+++
@@ -1,8 +0,0 @@
-#' @details
-#' The only function you're likely to need from cat is [cat()].
-#' Otherwise refer to the vignettes to see how to format the documentation.
-#' @import Rcpp
-#' @useDynLib mcgraph , .registration = TRUE
-#' @keywords internal
-"_PACKAGE"
-#> [1] "_PACKAGE"
